284a8a4c54f018fd8d13a5ccfe87fa121b6863ec
郑龙天 <zhenglongtian@outlook.com>
2018-06-04 21:36:06 +0800

    very evil
    i am an evil scientst, all things in this world is made up by me!

---
+++ components/creatures/earthCreatures/Chordata/Vertebrata/mammalia/primates/hominidae/human.py
@@ -0,0 +1,21 @@
+class Human():
+    def __init__(self):
+        self.head = None
+        self.neck = None
+        self.body = None
+        self.hands = [None, None, ]
+        self.dick = None
+        self.ass = None
+        self.feet = [None, None, ]
+
+    def fuck(self):
+        print("It is comfortable, while, not always...")
+
+    def happy(self):
+        pass
+
+    def angry(self):
+        pass
+
+    def sad(self):
+        pass
